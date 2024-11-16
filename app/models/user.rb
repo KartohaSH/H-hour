087e659d1224367b05ff553cf22db71cd6f4efcd
Denys Zumer <dendobdob@gmail.com>
2024-11-16 15:42:42 +0200

    Sign up is done

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# email:string
+# password_digest:string
+#
+# passwrd:string virtual
+# password_confirmation virtualpa
+
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid e-mail address"}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 end
