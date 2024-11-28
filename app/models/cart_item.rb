79b8de23f7416dcb311c18a692fdb1908d558cac
Denys Zumer <dendobdob@gmail.com>
2024-11-28 16:30:30 +0200

    hotfix

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,7 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
-  belongs_to :product, :order
+  belongs_to :product
+  belongs_to :order, optional: true 
+  
   validates :quantity, presence: true
 end
